eb4057dcdd15335462dfc353752fbd403a1c89c5
huy44551998 <dangnguyenxuanhuy92@gmail.com>
2020-07-22 18:40:02 +0700

    in thong bao

--- app/src/main/java/com/example/firstapp13072020/MainActivity.java
+++ app/src/main/java/com/example/firstapp13072020/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.firstapp13072020;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int value;
+       value =  tinhTong(5,6);
+        Log.d("BBB",String.valueOf(value));
+        String msg= "dang hoc bai";
+        inThongBao(msg);
+    }
+    private int tinhTong(int a, int b){
+        int ketqua = a+b;
+        return ketqua;
+    }
+    private void inThongBao(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 }
