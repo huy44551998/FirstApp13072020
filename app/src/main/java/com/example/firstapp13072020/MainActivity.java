923326f9c30b96a00f1136e164c6a76cce3c0082
huy44551998 <dangnguyenxuanhuy92@gmail.com>
2020-07-24 18:19:14 +0700

    giao dien game loto

--- app/src/main/java/com/example/firstapp13072020/MainActivity.java
+++ app/src/main/java/com/example/firstapp13072020/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,11 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int value;
+        double random = Math.random();
        value =  tinhTong(5,6);
-        Log.d("BBB",String.valueOf(value));
+        Random random1 = new Random();
+        //21>101
+        int value1 = random1.nextInt(81)+21;
+        random1.nextBoolean();
+        Log.d("BBB","Floor is round down"+random);
         String msg= "dang hoc bai";
         inThongBao(msg);
+
     }
+
     private int tinhTong(int a, int b){
         int ketqua = a+b;
         return ketqua;
