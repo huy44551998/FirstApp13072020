24683d8251f47aab8eaeff490136bd1caffaade8
huy44551998 <dangnguyenxuanhuy92@gmail.com>
2020-07-24 19:32:17 +0700

    game loto co ban

--- app/src/main/java/com/example/firstapp13072020/MainActivity.java
+++ app/src/main/java/com/example/firstapp13072020/MainActivity.java
@@ -3,34 +3,69 @@ package com.example.firstapp13072020;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mEdtmin, mEdtmax;
+    Button mBtnRandom;
+    TextView mTvResult;
+    String mValue="";
+    Random random = new Random();
+    int n=0,a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int value;
-        double random = Math.random();
-       value =  tinhTong(5,6);
-        Random random1 = new Random();
-        //21>101
-        int value1 = random1.nextInt(81)+21;
-        random1.nextBoolean();
-        Log.d("BBB","Floor is round down"+random);
-        String msg= "dang hoc bai";
-        inThongBao(msg);
+        mBtnRandom = findViewById(R.id.randombtn);
+        mEdtmin = findViewById(R.id.minNumber);
+        mEdtmax = findViewById(R.id.maxNumber);
+        mTvResult = findViewById(R.id.result);
+        mBtnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String textSmin = mEdtmin.getText().toString();
+               String textSmax = mEdtmax.getText().toString();
+                if(textSmax.isEmpty()||textSmin.isEmpty()){
+                    Toast.makeText(MainActivity.this, "please enter the numbers", Toast.LENGTH_SHORT).show();
+                return;
+                }
+                int min = Integer.parseInt(textSmin);
+                int max = Integer.parseInt(textSmax);
+                if(min>=max){
+
+                    max = min+1;
+                }
+                if(max==0){
+                    textSmax = "1";
+                    max=1;
+                    mEdtmax.setText(textSmax);
+                    textSmin="0";
+                    min=0;
+                    mEdtmin.setText(textSmin);
+
+                }
+
+
+                 n = random.nextInt((max-min)+1)+min;
+                mValue =mValue+n+"-";
+
+                mTvResult.setText(mValue);
+
+            }
+        });
+
 
     }
 
-    private int tinhTong(int a, int b){
-        int ketqua = a+b;
-        return ketqua;
-    }
-    private void inThongBao(String msg){
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
-    }
+
 }
